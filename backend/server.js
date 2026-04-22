0be01ca8cc0d63cfa53f525e5797acbeef922dc7
gourav <gourav@auvo.io>
2026-04-22 12:53:02 +0530

    feat: added auth controllers and auth routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
 
 
 dotenv.config();
@@ -12,6 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Server running on port ${process.env.PORT || 5000}`)
