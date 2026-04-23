3d6c1ea9b61500c24277374e752fd618848c367c
gourav <gourav@auvo.io>
2026-04-23 11:56:43 +0530

    feat: built token end point in oauth controller

--- backend/controllers/oauthcontroller.js
+++ backend/controllers/oauthcontroller.js
@@ -2,6 +2,8 @@ import asyncHandler from "express-async-handler";
 import crypto from "crypto";
 import Client from "../models/clientModel.js";
 import AuthCode from "../models/authCodeModel.js";
+import jwt from "jsonwebtoken";
+
 
 const authorize = asyncHandler(async (req, res) => {
   const { client_id, redirect_uri, response_type, state } = req.query;
@@ -51,4 +53,51 @@ const authorize = asyncHandler(async (req, res) => {
   res.redirect(redirectUrl);
 });
 
-export default authorize;
+const token = asyncHandler(async(req, res) => {
+  const {client_id, code, redirect_uri } = req.body;
+
+  // validate required params
+  if(!client_id || !code || !redirect_uri) {
+    res.status(400);
+    throw new Error("Invalid request");
+  }
+
+  //validate client
+  const client = await Client.findOne({clientId: client_id});
+
+  if(!client) {
+    res.status(404);
+    throw new Error("Client not found");
+  }
+
+  //validate auth code
+  const authCode = await AuthCode.findOne({code})
+
+  if(!authCode) {
+    res.status(404);
+    throw new Error("Auth code not found");
+  }
+
+  //validate code ownership
+  if(authCode.clientId !== client_id || authCode.redirectUri !== redirect_uri) {
+    res.status(400);
+    throw new Error("Invalid code details");
+  }
+
+  //check expiry
+  if(authCode.expiresAt < new Date()) {
+    res.status(400);
+    throw new Error("Auth code expired");
+  }
+
+  //generate access token
+  const accessToken = jwt.sign({userId: authCode.userId, clientId: client_id}, process.env.JWT_SECRET, {expiresIn: "15m"});
+
+  //delete auth code
+  await AuthCode.deleteOne({code});
+
+  //send response
+  res.json({access_Token: accessToken, token_type: "Bearer", expires_in: 900})
+})
+
+export {authorize, token};
--- backend/routes/oauthRoutes.js
+++ backend/routes/oauthRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import authorize from "../controllers/oauthcontroller.js";
+import {authorize, token} from "../controllers/oauthcontroller.js";
 
 const router = express.Router();
 
 router.get("/authorize", authorize);
+router.post("/token", token);
 
 export default router;
