aef5f9ca962b5ae628c307fd26355fd8544e6e5a
gourav <gourav@auvo.io>
2026-04-28 10:13:25 +0530

    fix: baseQuery with baseQueryWithReauth

--- frontend/slices/apiSlice.js
+++ frontend/slices/apiSlice.js
@@ -8,9 +8,32 @@ const baseQuery = fetchBaseQuery({baseUrl: "http://localhost:3000/api", credenti
     return headers;
 }});
 
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions);
+    //if access token expired
+    if (result.error && result.error.status === 401) {
+        console.log("Access token expired");
+
+        const refreshResult = await baseQuery({
+            url: '/oauth/refresh',
+            method: "POST",
+            body: {
+                client_id: "abc123",
+                refresh_token: localStorage.getItem("refresh_token")
+            }
+        }, api, extraOptions)
+        if(refreshResult?.data) {
+            api.dispatch(setToken(refreshResult.data))
+        } else {
+            api.dispatch(logout())
+        }
+    }
+    return result;
+}
+
 export const apiSlice = createApi({
     reducerPath: "api",
-    baseQuery,
+    baseQuery: baseQueryWithReauth,
     typeTags: ["Auth","User"],
     endpoints: () => ({})
 })
